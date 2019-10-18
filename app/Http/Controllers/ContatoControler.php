6fd18ed4a406345d067517ca18cfe975cd593c6b
Felipe Alves de Oliveira <feliperj629@gmail.com>
2019-10-18 16:51:08 -0300

    Controler e Model

--- app/Http/Controllers/ContatoControler.php
+++ app/Http/Controllers/ContatoControler.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contato;
 
 class ContatoControler extends Controller
 {
@@ -12,6 +13,13 @@ class ContatoControler extends Controller
     		(object)["nome"=>"Maria", "tel"=>"123456"],
     		(object)["nome"=>"Pedro", "tel"=>"654321"]
     	];
+
+    	$contato = new Contato();
+    	$conn = $contato->lista();
+
+    	dd($conn->nome);
+
+
     	return view('contato.index', compact('contatos'));
     }
     public function criar(Request $req)
